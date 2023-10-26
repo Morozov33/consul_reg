9a5cbcdbb0293f0ca846476e50a9ca6a5f23f5a8
Dmitrii Morozov <33morozov@gmail.com>
2023-10-26 16:41:45 +0700

    add few queury

--- app/handlers.py
+++ app/handlers.py
@@ -9,7 +9,7 @@ from loguru import logger
 
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, UnexpectedAlertPresentException
 from selenium.webdriver.common.action_chains import ActionChains
 
 from sender import sent_message_into_queue
@@ -17,9 +17,8 @@ from driver import create_driver
 from twocaptcha import TwoCaptcha
 
 
-def consul_handler():
-    driver = create_driver()
-    link = os.getenv("CONSUL_LINK", "")
+def consul_handler(name: str, link: str, driver):
+
     driver.get(link)
     actions = ActionChains(driver)
     wait = WebDriverWait(driver, 10)
@@ -29,9 +28,9 @@ def consul_handler():
     actions.move_to_element(img)
     actions.perform()
     # driver.save_screenshot("./screens/0.png")
-    captcha_path = get_captcha(img)
+    captcha_path = get_captcha(img, name)
     solved_captcha = get_solved_captcha(captcha_path)
-    logger.info(f"Captcha image:{captcha_path}. Captcha solve: {solved_captcha}")
+    logger.info(f"Captcha image:{captcha_path}. Captcha solve: {solved_captcha} for {name}")
 
     captcha_response = driver.find_element(By.XPATH, '//*[@id="ctl00_MainContent_txtCode"]')
     captcha_response.send_keys(solved_captcha)
@@ -39,30 +38,43 @@ def consul_handler():
     actions.perform()
     # driver.save_screenshot("./screens/1.png")
     captcha_response.send_keys(selenium.webdriver.Keys.ENTER)
-    time.sleep(10)
-    footer = driver.find_element(By.XPATH, '//*[@id="footer"]')
-    actions.move_to_element(footer)
-    # driver.save_screenshot("./screens/2.png")
+    time.sleep(5)
+    if name.endswith("Antalya"):
+        footer = driver.find_element(By.XPATH, '//*[@id="footer"]')
+        actions.move_to_element(footer)
+        # driver.save_screenshot("./screens/2.png")
+
     try:
         driver.find_element(By.XPATH, '//*[@id="ctl00_MainContent_lblCodeErr"]')
     except NoSuchElementException:
         continue_button = driver.find_element(By.XPATH, '//*[@id="ctl00_MainContent_ButtonB"]')
         continue_button.click()
-        time.sleep(10)
-        driver.save_screenshot(f"./screens/{str(datetime.now().date())}.png")
-        sent_message_into_queue(f"Log from consul_reg: {str(datetime.now().date())} | Successed update query")
-        logger.info("Successed update query")
+        time.sleep(5)
+        driver.save_screenshot(f"./screens/{name}_{str(datetime.now().date())}.png")
+        sent_message_into_queue(f"Log from consul_reg: {str(datetime.now().date())} | Successed update query for {name}")
+        logger.info(f"Successed update query for {name}")
+    except UnexpectedAlertPresentException:
+        actions.send_keys(selenium.webdriver.Keys.ENTER)
+        actions.perform()
+        time.sleep(5)
+        continue_button = driver.find_element(By.XPATH, '//*[@id="ctl00_MainContent_ButtonB"]')
+        continue_button.click()
+        time.sleep(5)
+        driver.save_screenshot(f"./screens/{name}_{str(datetime.now().date())}.png")
+        sent_message_into_queue(
+            f"Log from consul_reg: {str(datetime.now().date())} | Successed update query for {name}")
+        logger.info(f"Successed update query for {name}")
     else:
-        driver.save_screenshot(f"./screens/wrong_{str(datetime.now().date())}.png")
-        sent_message_into_queue(f"Log from consul_reg: {str(datetime.now().date())} | Wrong capture")
-        logger.error("Wrong capture")
-
-    driver.close()
+        driver.save_screenshot(f"./screens/wrong_{name}_{str(datetime.now().date())}.png")
+        sent_message_into_queue(f"Log from consul_reg: {str(datetime.now().date())} | Wrong capture for {name}")
+        logger.error(f"Wrong capture for {name}")
 
 
-def get_captcha(img) -> str:
+
+
+def get_captcha(img, name: str) -> str:
     # download the image as screenshot
-    file_name = f"captcha_{str(datetime.now().date())}.png"
+    file_name = f"captcha_{name}_{str(datetime.now().date())}.png"
     file_path = os.path.join(os.getenv("CAPTCHA_IMG_PATH", ""), file_name)
     img.screenshot(file_path)
     return file_path
--- app/main.py
+++ app/main.py
@@ -2,13 +2,24 @@ import os
 
 from loguru import logger
 from handlers import consul_handler
+from driver import create_driver
 
 LOG_PATH = os.environ.get("LOG_PATH")
+LINKS = list(os.getenv("CONSUL_LINKS", "").split(","))
+NAMES = list(os.getenv("NAMES", "").split(","))
+names_links = {
+    NAMES[0]: LINKS[0],
+    NAMES[1]: LINKS[1],
+}
 
 
 if __name__ == '__main__':
     try:
         logger.add(LOG_PATH)
-        consul_handler()
+        driver = create_driver()
+        for name in names_links:
+            consul_handler(name, names_links[name], driver)
     except Exception as e:
         logger.exception(e)
+    finally:
+        driver.close()
